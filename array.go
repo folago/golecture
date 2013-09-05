46f3ac758091cd0dd3ef1b31cf4ac0aefa060075
Giacomo Tartari <giacomo.tartari@uit.no>
2013-09-05 18:06:01 +0200

    some changes

--- array.go
+++ array.go
@@ -7,9 +7,11 @@ var f [3]*float64
 
 func main() {
 	primes := [...]int{2, 3, 5, 7, 11, 13, 17}
-	a := [...]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Println(primes)
+
+	fmt.Println("primes", primes)
+
+	fmt.Println("b", b)
+
+	fmt.Println("f", f)
 
 }
